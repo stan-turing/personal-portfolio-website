90a4e9e74009efa249270774fcbb1729d4f44bed
stan-turing <stan-turing@proton.me>
2024-12-18 11:46:43 +0100

    Fixed Menu Autoclose on click of button

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ hamburger.onclick = function() {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
+const menuButtons = document.querySelectorAll(".nav-bar a");
+menuButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const navBar = document.querySelector(".nav-bar");
+        navBar.classList.remove("active"); // Menü schließen
+    });
+});
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
